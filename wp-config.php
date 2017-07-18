2c097db8623e8f694c081cb7f346549d5fcd5387
Samuel <citysite1025@gmail.com>
2017-07-18 12:24:50 +0800

    Update file contnet

--- wp-config.php
+++ wp-config.php
@@ -14,22 +14,36 @@
  * @package WordPress
  */
 
+$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
+
+/** The name of the database for WordPress */
+define('DB_NAME', trim($url['path'], '/'));
+
+/** MySQL database username */
+define('DB_USER', $url['user']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $url['pass']);
+
+/** MySQL hostname */
+define('DB_HOST', $url['host']);
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 // define('DB_NAME', 'talk_ux_com_1');
-define('DB_NAME', 'd1h9bnnc3g13vt');
+// define('DB_NAME', 'd1h9bnnc3g13vt');
 
 /** MySQL database username */
 // define('DB_USER', 'talkuxcom1');
-define('DB_USER', 'wrfotdgsptnuxy');
+// define('DB_USER', 'wrfotdgsptnuxy');
 
 /** MySQL database password */
 // define('DB_PASSWORD', 'sfnAL?3D');
-define('DB_PASSWORD', '8f680de5bfa3e30e196ab4e8215152a783fcefa51cc6de7752e3718899e2b4bf');
+// define('DB_PASSWORD', '8f680de5bfa3e30e196ab4e8215152a783fcefa51cc6de7752e3718899e2b4bf');
 
 /** MySQL hostname */
 // define('DB_HOST', 'mysql.talk-ux.com');
-define('DB_HOST', 'ec2-23-23-244-83.compute-1.amazonaws.com:5432');
+// define('DB_HOST', 'ec2-23-23-244-83.compute-1.amazonaws.com:5432');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -46,14 +60,23 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '(xc4vLsPj1p_EzYqDO)k2&j&F8duXloi7tbd@+R(dNjj6vlX_c(PBy3%:q@c@+F~');
-define('SECURE_AUTH_KEY',  'DYGj%:1&h:7nwk;A_h0yg;CrEK06/OSg1i|w00tO%Kl1"7Q5x&pLzM1VJ~bB9d##');
-define('LOGGED_IN_KEY',    'a)|pW?D;XPlbLBfovWw1SECIjj@y8f$k8UyyUXHk1twxPDkhfe:dT)1:L"6GchN2');
-define('NONCE_KEY',        'uS23jlSWVZF%OmuMKUQ&@fSa0^lMESd(nrga^?4RTklfNi3waznIW37w;c5mLhBN');
-define('AUTH_SALT',        'nsITuEi;/HaXB;hjK("I:6Xs5VWI0+Etvm@ZT3;xuh/p4YGk^&`^_;VP2&8&yI!L');
-define('SECURE_AUTH_SALT', 'e2PX^U*owFn%Fzw7FZD6*A9_pbGYlu+)!wHIvZ)J@QefQKJ~3jgGw!j"fz"OE!5@');
-define('LOGGED_IN_SALT',   '!gbeTz+/*Dq~#$??LNv+lswPpstfd(LFZMjME:rj@0:B1_DM"o2@;O%HX39CK%iU');
-define('NONCE_SALT',       'GIJ@1`NxImKQ+xbrJps/_/D:GiUrJXsF0+pbaY&tCPAAcS&KWV!lRjp+"z?2;JGA');
+// define('AUTH_KEY',         '(xc4vLsPj1p_EzYqDO)k2&j&F8duXloi7tbd@+R(dNjj6vlX_c(PBy3%:q@c@+F~');
+// define('SECURE_AUTH_KEY',  'DYGj%:1&h:7nwk;A_h0yg;CrEK06/OSg1i|w00tO%Kl1"7Q5x&pLzM1VJ~bB9d##');
+// define('LOGGED_IN_KEY',    'a)|pW?D;XPlbLBfovWw1SECIjj@y8f$k8UyyUXHk1twxPDkhfe:dT)1:L"6GchN2');
+// define('NONCE_KEY',        'uS23jlSWVZF%OmuMKUQ&@fSa0^lMESd(nrga^?4RTklfNi3waznIW37w;c5mLhBN');
+// define('AUTH_SALT',        'nsITuEi;/HaXB;hjK("I:6Xs5VWI0+Etvm@ZT3;xuh/p4YGk^&`^_;VP2&8&yI!L');
+// define('SECURE_AUTH_SALT', 'e2PX^U*owFn%Fzw7FZD6*A9_pbGYlu+)!wHIvZ)J@QefQKJ~3jgGw!j"fz"OE!5@');
+// define('LOGGED_IN_SALT',   '!gbeTz+/*Dq~#$??LNv+lswPpstfd(LFZMjME:rj@0:B1_DM"o2@;O%HX39CK%iU');
+// define('NONCE_SALT',       'GIJ@1`NxImKQ+xbrJps/_/D:GiUrJXsF0+pbaY&tCPAAcS&KWV!lRjp+"z?2;JGA');
+
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 
 /**#@-*/
 
